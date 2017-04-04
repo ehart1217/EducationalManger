ea1caeb89af36a2388f7354fbadcdf92ad473db1
ehart1217 <ehart@ehartsdeMacBook-Pro.local>
2017-04-05 01:08:06 +0800

    college page is going on

---
+++ app/src/main/java/com/rachel/manager/ui/CollegeActivity.java
@@ -0,0 +1,22 @@
+package com.rachel.manager.ui;
+
+import android.os.Bundle;
+
+import com.rachel.manager.R;
+import com.rachel.manager.base.BaseActivity;
+
+/**
+ * 学院列表页面
+ * Created by Rachel on 17/4/5.
+ */
+
+public class CollegeActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_college_list);
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
